6d2283841ca46b0ef19ab5158883ad7d13861c31
sennierer <matthias.schloegl@oeaw.ac.at>
2023-01-12 10:23:00 +0000

    adds bulk retrieve for occupations

--- intavia_backend/main_v2.py
+++ intavia_backend/main_v2.py
@@ -109,6 +109,26 @@ async def retrieve_occupation_v2(occupation_id: str):
     return {"_results": flatten_rdf_data(res)}
 
 
+@router.post(
+    "/api/vocabularies/occupations/retrieve",
+    response_model=PaginatedResponseVocabularyEntries,
+    response_model_exclude_none=True,
+    tags=["Vocabulary endpoints"],
+    description="Endpoint that allows to bulk retrieve occupations when IDs are known.",
+)
+async def bulk_retrieve_voc_occupations(
+    ids: RequestID,
+    query: QueryBase = Depends(),
+):
+    query_dict = asdict(query)
+    query_dict["ids"] = ids.id
+    res = get_query_from_triplestore_v2(query_dict, "bulk_retrieve_occupations_v2_1.sparql")
+    res = flatten_rdf_data(res)
+    pages = math.ceil(int(res[0]["count"]) / query.limit) if len(res) > 0 else 0
+    count = int(res[0]["count"]) if len(res) > 0 else 0
+    return {"page": query.page, "count": count, "pages": pages, "results": res}
+
+
 @router.get(
     "/api/vocabularies/role/search",
     response_model=PaginatedResponseVocabularyEntries,
